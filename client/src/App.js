fc94d55c7505bb544d82d0145425d22fe6328246
sunprodefi <sunprodefi@gmail.com>
2025-03-28 22:22:14 +0800

    修正导入路径以确保组件正确加载

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "../client/src/pages/Register";
-import Login from "../client/src/pages/Login";
-import Dashboard from "../client/src/pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
