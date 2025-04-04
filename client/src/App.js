1c37afc01218d4cdaddbe1b3c2fafee4936dae4c
sunprodefi <sunprodefi@gmail.com>
2025-04-04 14:27:07 +0800

    重构 App 组件，更新路由配置以重定向到仪表板，新增全局样式文件，优化 TripDetail 组件样式，增强用户体验

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import TripForm from "./components/TripForm";
-import TripDetail from "./components/TripDetail";
+import TripForm from "./pages/TripForm";
+import TripDetail from "./pages/TripDetail";
+import "./styles/App.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/trips/new" element={<TripForm />} />
-        <Route path="/trips/:id/edit" element={<TripForm />} />
-        <Route path="/trips/:id" element={<TripDetail />} />
-      </Routes>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/trips/new" element={<TripForm />} />
+          <Route path="/trips/:id/edit" element={<TripForm />} />
+          <Route path="/trips/:id" element={<TripDetail />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
